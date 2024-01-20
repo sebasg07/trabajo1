e70e43df3736e521694b55204fb6c88b701ea923
sebas07 <sebas7077@gmail.com>
2024-01-20 15:31:02 -0500

    ejercicio uno completado

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,33 @@
+#definir la funcion
 def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
+    # gradar un valor tipo float 
+    num1 = float(input("Ingrese el primer número: ")) #convertir el string en float agregando el input
+    # gradar un valor tipo float 
+    num2 = float(input("Ingrese el segundo número: ")) #convertir el string en float agregando el input
+    # escoger una operación a la cual se va a realizar 
     operacion = input("Ingrese la operación (+, -, *, /): ")
-
+    # condicion donde el usuario elige suma
     if operacion == '+':
-        resultado = num + num2
+        resultado = num1 + num2 #error en el num1
+    # condicion donde el usuario elige resta
     elif operacion == '-':
+    # operacion si se cumple la condicion
         resultado = num1 - num2
+    # condicion donde el usuario elige multiplicación
     elif operacion == '*':
+    # operacion si se cumple la operacion
         resultado = num1 * num2
+    # condicion donde el usuario elige division
     elif operacion == '/':
-        resultado = num / num2
+    # operacion sis e cumple la condicion
+        resultado = num1 / num2 #error en el num1
+    # si no se cumple las condiciones 
     else:
+        # resultado en caso de que no se compla la condicion
         resultado = "Operación no válida"
+    # imprimir el resultado de las operaciónes.
+    print(resultado) #error de las comillas y en la variable dentro del ()
 
-    print("Resultado: "resultado)
+calculadora() # error en el nombre de la funcion 
 
-calculdora()
+# es una calculadora que nos permite realizar funciones de suma resta multiplicacion y division
