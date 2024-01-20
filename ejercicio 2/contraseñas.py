8858dda84c9d95428d8c98949dc30caeb4450bc7
sebas07 <sebas7077@gmail.com>
2024-01-20 15:49:59 -0500

    ejercicio completado

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,14 @@
-import rando
+# es una libreria aletoria aleatoria 
+import random  # Se corrigió la importación de random
+# es una libreria de texto
 import string
-
-generar_contraseña(longitud=8):
+# define la funcion y tiene un argumento opcional 
+def generar_contrasena(longitud=8):  # Se corrigió el nombre de la función y se agregó el tipo de parámetro
+   #crea una cadena de caracteres que contiene letras entre mayusculas y minusculas
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
-
-print(generar_contrasena())
+   # crea una contraseña aleatoria
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))  #Se corrigió el nombre de la variable y la palabra 'return'
+    # devuelve la contraseña creada por la funcion
+    return contrasena
+# imprime la contraseña generada
+print(generar_contrasena())  # Se corrigió el nombre de la función en la llamada
