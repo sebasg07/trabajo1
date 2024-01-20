6ea4122638cf820cd425b0cb31af5d6da2c05aec
sebas07 <sebas7077@gmail.com>
2024-01-20 16:07:48 -0500

    ejercicio completado

--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -1,7 +1,10 @@
-def contar_palabra(texto, palabra)
+# definir la funcion
+def contar_palabra(texto, palabra): # faltaba colocar el caracter :
     return texto.lower().split().count(palabra.lower())
-
+# definición de una cadena de texto
 texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
+# definir la palabra buscada en el texto
 palabra_buscada = "texto"
+# imprimir el mensaje buscado
+print(f"La palabra '{palabra_buscada}' aparece {contar_palabra(texto, palabra_buscada)} veces.") # faltaba cerrar la llave, cerrar la comilla simple en la variable y abrir la otra llave. 
 
-print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")
