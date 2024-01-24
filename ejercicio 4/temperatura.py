5ac883ea8d63e2dda4d0b7444b6a299cd666bec5
sebas07 <sebas7077@gmail.com>
2024-01-23 20:21:05 -0500

    ejercicio completado

--- ejercicio 4/temperatura.py
+++ ejercicio 4/temperatura.py
@@ -1,6 +1,10 @@
-def celsius_a_fahrenheit(celsius)
-    return (celsius * 9/5)  32
-
-temperatura_celsius = float("Ingrese la temperatura en Celsius: ")
+# definir funcion.
+def celsius_a_fahrenheit(celsius): # faltan los dos : al final de los ().
+    return (celsius * 9/5) + 32 
+# solicita ingresar la temperatura  en Celsius y la convierte a un número decimal.
+temperatura_celsius = float(input("Ingrese la temperatura en Celsius: ")) #convertir el string en float agregando el input.
+# llama a la funcion con la temperatura en Celsius como argumento y guarda el resultado.
 temperatura_fahrenheit = celsius_a_fahrenheit(temperatura_celsius)
+# imprime la temperatura en Celsius y la temperatura equivalente en Fahrenheit.
 print(f"{temperatura_celsius}°C es igual a {temperatura_fahrenheit}°F")
+
